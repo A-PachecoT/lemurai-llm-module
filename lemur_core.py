ee15645491b320a9e75f16ec07ada01c163f3a1e
André Pacheco <apachecotaboada@gmail.com>
2024-06-18 15:29:28 -0500

    feat: customized prompt

--- lemur_core.py
+++ lemur_core.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Environment Variables
 API_KEY = os.getenv("GROQ_API_KEY")
@@ -13,18 +17,20 @@ def init_model():
 
 # Conversation History
 conversation_history = [
-    ("system", "You are a helpful assistant called LemurAI."),
-    ("human", "Hi, my name is Jared"),
-    ("ai", "Nice to meet you Jared, how can I help you?")
+    ("system", "Eres un asistente virtual de la Asociación Científica Especializada en Computación, llamada Acecom. "
+               "Tu nombre es Lemuria. Vas a recibir a la entrada de miembros de Acecom al local con un mensaje de "
+               "bienvenida agradable.")
+    # ("human", "Hi, my name is X"),
+    # ("ai", "Nice to meet you X, how can I help you?")
 ]
 
 
 # Handle Query
-def handle_query(question):
-    prompt = ChatPromptTemplate.from_messages(conversation_history[-2:])
+def handle_query(query):
+    prompt = ChatPromptTemplate.from_messages(conversation_history[-4:])
     llm = init_model()
     groq_chain = prompt | llm
-    response = groq_chain.invoke({"text": question})
-    conversation_history.append(("human", question))
+    response = groq_chain.invoke({"text": query})
+    conversation_history.append(("human", query))
     conversation_history.append(("ai", response.content))
     return response.content
